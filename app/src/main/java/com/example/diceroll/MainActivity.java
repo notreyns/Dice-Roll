f89cb54970174ef79a5d95d0fd775922a92665af
Reina <reina.khasanova@iaau.edu.kg>
2021-06-27 23:32:42 +0600

    ..

--- app/src/main/java/com/example/diceroll/MainActivity.java
+++ app/src/main/java/com/example/diceroll/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.diceroll;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,13 +9,14 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private Button btn;
     public static final Random randint = new Random();
     ImageView firstImage, secondImage;
-    int[] memory= new int[2];
+    int[] memory = new int[2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,30 +27,41 @@ public class MainActivity extends AppCompatActivity {
                 R.id.first_image_view);
         secondImage = (ImageView) findViewById(
                 R.id.second_image_view);
-        if (savedInstanceState != null){
-            secondImage.setImageResource(savedInstanceState.getInt("img1"));
-            firstImage.setImageResource(savedInstanceState.getInt("img2"));
-        }
 
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                int rand1 = getResources().getIdentifier("dice" + (randint.nextInt(6)+1), "drawable", "com.example.diceroll");
-                firstImage.setImageResource(rand1);
-                memory[0] = rand1;
-
-                int rand2= getResources().getIdentifier("dice" + (randint.nextInt(6)+1), "drawable", "com.example.diceroll");
-                secondImage.setImageResource(rand2);
-                memory[1] = rand2;
-            }
-        });
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                int rand1 = getResources().getIdentifier("dice" + (randint.nextInt(6)+1), "drawable", "com.example.diceroll");
+//                firstImage.setImageResource(rand1);
+//                memory[0] = rand1;
+//
+//                int rand2= getResources().getIdentifier("dice" + (randint.nextInt(6)+1), "drawable", "com.example.diceroll");
+//                secondImage.setImageResource(rand2);
+//                memory[1] = rand2;
+//            }
+//        });
     }
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
         savedInstanceState.putInt("img1", memory[0]);
         savedInstanceState.putInt("img2", memory[1]);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        secondImage.setImageResource(savedInstanceState.getInt("img1"));
+        firstImage.setImageResource(savedInstanceState.getInt("img2"));
+    }
+
+    public void generateRandom(View view) {
+        int rand1 = getResources().getIdentifier("dice" + (randint.nextInt(6)+1), "drawable", "com.example.diceroll");
+        firstImage.setImageResource(rand1);
+        memory[0] = rand1;
+
+        int rand2= getResources().getIdentifier("dice" + (randint.nextInt(6)+1), "drawable", "com.example.diceroll");
+        secondImage.setImageResource(rand2);
+        memory[1] = rand2;
     }
 }
